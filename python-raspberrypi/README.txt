acaffc4d821423ab0b3a2ea1209768a98dacbd67
Rui Filipe de Sousa Martins <ruimartinsptl@gmail.com>
2016-06-20 00:56:47 +0100

    Updated. Is working

---
+++ python-raspberrypi/README.txt
@@ -0,0 +1,51 @@
+PROJECT: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Sensors <--> Arduino <--> Raspberry Pi <--> Firebase <--> Web App
+
+RASPBERRYPI:
+Listen (with interrupts) PORT 20 (Team A) and PORT 21 (Team B).
+When an trigger happens, the internal variables (goal counters) is updated, and an message is sent by firebase
+done.
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+INSTALL ON RASPBIAN: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y python-dev python3-dev
+sudo apt-get install -y python-pip python3-pip
+sudo pip install bottle
+
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+GPIO ON OSMC: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+sudo apt-get update
+sudo apt-get install build-essential
+export ARCH=arm
+export CROSS_COMPILE=/usr/bin/
+
+By: https://discourse.osmc.tv/t/gpio-control-need-some-help/3246/4
+
+
+
+To install the latest development version from the project source code library:
+
+$ sudo apt-get install -y python-dev python3-dev
+$ sudo apt-get install -y mercurial
+$ sudo apt-get install -y python-pip python3-pip
+$ sudo apt-get remove -y python-rpi.gpio python3-rpi.gpio
+$ sudo pip install hg+http://hg.code.sf.net/p/raspberry-gpio-python/code#egg=RPi.GPIO
+$ sudo pip-3.2 install hg+http://hg.code.sf.net/p/raspberry-gpio-python/code#egg=RPi.GPIO
+
+To revert back to the default version in Raspbian:
+
+$ sudo pip uninstall RPi.GPIO
+$ sudo pip-3.2 uninstall RPi.GPIO
+$ sudo apt-get install python-rpi.gpio python3-rpi.gpio
+
+by: http://raspberrypi.stackexchange.com/questions/27407/importerror-no-module-named-gpio
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
--- python-raspberrypi/main.py
+++ python-raspberrypi/main.py
@@ -1,41 +1,98 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
 # PORTO 20 sempre que recebe um impulso a 1 vindo do arduino (ou do sensor), incrementa o numero de golos da equipa A e manda notificação
 # PORTO 21 sempre que recebe um impulso a 1 vindo do arduino (ou do sensor), incrementa o numero de golos da equipa B e manda notificação
 
 from bottle import route, run, template
+import json
+import requests
 import RPi.GPIO as GPIO
 __author__ = 'Rui Martins'
 
+try:
+    GPIO.cleanup()  # clean up GPIO
+except:
+    pass
+
+url = "https://sweltering-torch-9311.firebaseio.com/results.json"
+
+banner = """
+██╗  ██╗ █████╗ ██████╗ ██████╗ ██╗    ██╗ █████╗ ██████╗ ███████╗     ██████╗██╗████████╗██╗   ██╗
+██║  ██║██╔══██╗██╔══██╗██╔══██╗██║    ██║██╔══██╗██╔══██╗██╔════╝    ██╔════╝██║╚══██╔══╝╚██╗ ██╔╝
+███████║███████║██████╔╝██║  ██║██║ █╗ ██║███████║██████╔╝█████╗      ██║     ██║   ██║    ╚████╔╝
+██╔══██║██╔══██║██╔══██╗██║  ██║██║███╗██║██╔══██║██╔══██╗██╔══╝      ██║     ██║   ██║     ╚██╔╝
+██║  ██║██║  ██║██║  ██║██████╔╝╚███╔███╔╝██║  ██║██║  ██║███████╗    ╚██████╗██║   ██║      ██║
+╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝     ╚═════╝╚═╝   ╚═╝      ╚═╝
+
+"""  # http://patorjk.com/software/taag/#p=display&f=ANSI%20Shadow&t=HARDWARE%20CITY
+
+print banner
+
+print "GPIO VERSION:", GPIO.VERSION
+print "URL:", url
+
 # TODO: Usar Gevent-socketio para notificações em realtime
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # or PUD_DOWN ?
-GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # or PUD_DOWN ?
+GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # or PUD_UP ?
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # or PUD_UP ?
 
 info = {
     "team_a": {
         "name": "TEAM A",
-        "goals": 0
+        "goals": 0,
+        "players": [
+            {
+                "name": "Rui Martins"
+            },
+            {
+                "name": "Fábio Ferreira"
+            }
+        ]
     },
-    "team_a": {
+    "team_b": {
         "name": "TEAM B",
-        "goals": 0
+        "goals": 0,
+        "players": [
+            {
+                "name": "Francisco Mendes"
+            },
+            {
+                "name": "Miguel Almeida"
+            }
+        ]
     }
 }
 
 
-def _goal_team_a():
+def _send_score():
+    # Devia de se enviar o INFO derectamente
+    data = {
+        "team_a": info["team_a"]["goals"],
+        "team_b": info["team_b"]["goals"]
+    }
+    r = requests.put(url=url, data=json.dumps(data))
+    # print r.status_code
+    # print r.content
+
+
+def _goal_team_a(pin_number):
     # TODO: Usar Gevent-socketio para notificações em realtime
     print "GOAL A!"
     info["team_a"]["goals"] += 1
+    _send_score()
 
 
-def _goal_team_b():
+def _goal_team_b(pin_number):
     # TODO: Usar Gevent-socketio para notificações em realtime
     print "GOAL B!"
     info["team_b"]["goals"] += 1
+    _send_score()
 
 
 GPIO.add_event_detect(20, GPIO.FALLING, callback=_goal_team_a, bouncetime=300)
 GPIO.add_event_detect(21, GPIO.FALLING, callback=_goal_team_b, bouncetime=300)
+_send_score()
 
 # TODO: ...
 
@@ -46,7 +103,17 @@ def index():
                     name_a=info["team_a"]["name"], goals_a=info["team_a"]["goals"],
                     name_b=info["team_b"]["name"], goals_b=info["team_b"]["goals"])
 
-run(host='localhost', port=8080)
+@route('/reset')
+def reset():
+    info["team_a"]["goals"] = 0
+    info["team_b"]["goals"] = 0
+    _send_score()
+
+@route('/menu')
+def menu():
+    return "to do..."
+
+run(host='0.0.0.0', port=80)
 
 # TODO: ...
 
